f60291dd77685affe207658ed55d30b597a8ad5d
Kaleb Wells <wells2kb@mail.uc.edu>
2026-04-24 22:02:12 -0400

    add dynamic background feature

---
+++ Assets/background_controller.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class background_controller : MonoBehaviour
+{
+    public GameObject defaultBackground;
+    public GameObject[] backgrounds;
+
+    int index = -1;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+      defaultBackground.SetActive(true);
+      foreach (GameObject background in backgrounds) {
+        background.SetActive(false);
+      }
+
+      InputSystem.actions.FindAction("Next").performed += _ => nextBackground();
+      InputSystem.actions.FindAction("Previous").performed += _ => prevBackground();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void nextBackground() {
+      updateBackground(index + 1);
+    }
+
+    void prevBackground() {
+      updateBackground(index - 1);
+    }
+
+    void updateBackground(int newIndex) {
+      Debug.Log(index);
+      Debug.Log(newIndex);
+
+      int oldIndex = index;
+      index = newIndex;
+
+      if (index != oldIndex) {
+        if (oldIndex == -1) {
+          defaultBackground.SetActive(false);
+        } else {
+          backgrounds[oldIndex].SetActive(false);
+        }
+
+        if (index < -1) {
+          index = backgrounds.Length - 1;
+          backgrounds[index].SetActive(true);
+        } else if (index == -1) {
+          defaultBackground.SetActive(true);
+        } else if (index < backgrounds.Length) {
+          backgrounds[index].SetActive(true);
+        } else {
+          index = -1;
+          defaultBackground.SetActive(true);
+        }
+      }
+    }
+}
